42d6d93817002b55e4efe53e0b08a02f241a8dee
fqlu <fqlu@i9i8.com>
2020-01-15 22:38:35 +0800

    add

--- dialogue.py
+++ dialogue.py
@@ -1,2 +1,15 @@
 # main py
+from lib.util import *;
 
+
+class Dialogue():
+    def __init__(self):
+        print("init class");
+
+    def run(self):
+        print(util.add(3,4));
+        print("task begin!");
+
+if __name__ == "__main__":
+    main = Dialogue();
+    main.run();
--- lib/util.py
+++ lib/util.py
@@ -1,1 +1,9 @@
 # tool function
+
+
+class util:
+    def __init__(self):
+        print("init Util class");
+
+    def add(a, b):
+        return a + b;
